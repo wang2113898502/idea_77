9e206d1ae7e8ccbfc192b1b7edd6140fef2ed505
binbin <762142281@qq.com>
2018-12-29 23:14:28 +0800

    第一次提交

---
+++ idea_plus/src/HelloWorld.java
@@ -0,0 +1,13 @@
+/**
+ * @Author ：王伟彬
+ * @Date ：Created in 23:13 2018/12/29
+ * @Description：
+ * @Version: $
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+    }
+}
